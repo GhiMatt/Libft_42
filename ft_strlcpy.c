c382dc0cfa863c30b2bbedbddcf589e73adbb353
GhiMatt <125501445+GhiMatt@users.noreply.github.com>
2024-03-07 22:23:55 +0100

    string functions

---
+++ ft_strlcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mghibell <mghibell@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 22:16:17 by mghibell          #+#    #+#             */
+/*   Updated: 2024/03/07 22:16:17 by mghibell         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t  ft_strlcpy(char *to, const char *from, size_t size)
+{
+    size_t l;
+    size_t c;
+
+    l = 0;
+    c = ft_strlen(from);
+    if (size == 0)
+        return(c);
+    while (from[l] && l < size -1)
+    {
+        to[l] = from[l];
+        l++;
+    }
+    to[l] = '\0';
+    return(c);
+
+}
