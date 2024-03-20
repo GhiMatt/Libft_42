43e1acb52a76de592bc30c6e0bf6b09b3463fb27
GhiMatt <125501445+GhiMatt@users.noreply.github.com>
2024-03-20 23:04:45 +0100

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-int ft_memcmp(const void *p1, const void *p2m size_t s)
+int ft_memcmp(const void *p1, const void *p2, size_t s)
 {
     unsigned char *s1;
     unsigned char *s2;
